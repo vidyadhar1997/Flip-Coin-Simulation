c83f4033bbf9f2764cea4ac45974cb09ea668346
vidyadhar1997 <vidyadharhudge@gmail.com>
2020-08-14 05:30:56 -0700

    Added:-showing number of times tails or head wins

--- Flip-Coin-Simulator.sh
+++ Flip-Coin-Simulator.sh
@@ -1,12 +1,39 @@
 #!/bin/bash -x
 
-echo "welcome to flipCoin simulator game"
+read -p "Enter the number of flips : " count1
 
-toss=$((RANDOM%2))
+Head=1
+Tail=1
+count=1
 
-if [ $toss -eq 0 ]
-then
-     echo head
-else
-     echo tail
-fi
+while [[ $count -le $count1 ]] && [[ $count -gt 0 ]]
+do
+
+   toss=$(( RANDOM%2 ))
+   if [ $toss -eq 1 ]
+   then
+        echo "head" $Head
+        (( Head ++ ))
+   else
+        echo "tail" $Tail
+        (( Tail++ ))
+   fi
+   (( count ++ ))
+
+done
+
+   if [ $Head -gt $Tail ]
+   then
+        echo "Head is win"
+   elif [ $Tail -eq $Head ]
+   then
+        echo "match is tie"
+   else
+        echo "Tail is Win"
+   fi
+
+
+
+
+
+
